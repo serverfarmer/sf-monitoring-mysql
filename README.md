3ce51a9e1ec4721e75cd5c8ea2fc3dca424f8dc8
Tomasz Klim <github@tomaszklim.pl>
2016-09-15 17:03:43 +0200

    first version

---
+++ README.md
@@ -0,0 +1,5 @@
+sf-monitoring-mysql extension installs newrelic-mysql-plugin with fully
+automated configuration for local mysql database server.
+
+It is compatible with all Debian / Ubuntu versions and architectures,
+for which Java is available.
---
+++ setup.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+/opt/farm/scripts/setup/role.sh sf-monitoring-newrelic
+
+if [ ! -s /etc/local/.config/newrelic.license ]; then
+	echo "skipping newrelic-mysql-plugin configuration (no license key configured)"
+	exit 0
+elif [ ! -f /etc/mysql/debian.cnf ]; then
+	echo "skipping newrelic-mysql-plugin configuration (no mysql-server detected)"
+	exit 0
+elif [ "`which java`" = "" ]; then
+	echo "skipping newrelic-mysql-plugin configuration (no java detected)"
+	exit 0
+fi
+
+path=/opt/newrelic
+mkdir -p $path
+
+VERSION="2.0.0"
+LOCAL="newrelic_mysql_plugin-$VERSION"
+
+if [ ! -d $path/mysql ]; then
+	cd $path
+	wget "https://github.com/newrelic-platform/newrelic_mysql_java_plugin/raw/master/dist/$LOCAL.tar.gz"
+	tar xzf $LOCAL.tar.gz
+	rm -f $LOCAL.tar.gz
+	mv $LOCAL mysql
+	cd mysql
+
+	license="`cat /etc/local/.config/newrelic.license`"
+	password="`cat /etc/mysql/debian.cnf |grep password |tail -n1 |sed s/password\ =\ //g`"
+
+	cat $path/mysql/config/newrelic.template.json |sed s/YOUR_LICENSE_KEY_HERE/$license/g >$path/mysql/config/newrelic.json
+	cat $path/mysql/config/plugin.template.json |sed \
+		-e s/USER_NAME_HERE/debian-sys-maint/g \
+		-e s/USER_PASSWD_HERE/$password/g \
+		-e s/Localhost/`hostname`/g \
+		>$path/mysql/config/plugin.json
+	chmod 0600 $path/mysql/config/*.json
+
+	echo "#!/bin/sh\n\ncd $path/mysql && sudo -b -u newrelic java -Xmx128m -jar plugin.jar >>plugin.log" >$path/mysql/start.sh
+	touch $path/mysql/plugin.log
+	chmod +x $path/mysql/start.sh
+	chown -R newrelic:newrelic $path/mysql
+
+	$path/mysql/start.sh
+	echo "newrelic-mysql-plugin successfully installed"
+fi
+
+if [ "`cat /etc/rc.local /etc/local/*.sh 2>/dev/null |grep $path/mysql/start.sh`" = "" ]; then
+	echo "##############################################################################"
+	echo "# now add $path/mysql/start.sh script to your preferred boot trigger #"
+	echo "##############################################################################"
+fi
